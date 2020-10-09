31470e5a3af58d4f48abc40a1d8d9bda8c5e4cb2
张威 <897289382@qq.com>
2020-10-09 17:25:32 +0800

    添加字段

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -9,12 +9,13 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
-        [Display(Name ="Release Date")]
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
